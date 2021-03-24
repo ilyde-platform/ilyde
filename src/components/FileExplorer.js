5808422e1444dba999a23e1d2658e6e53e02d233
Alex Piacentini <piacentini.alex@gmail.com>
2021-03-24 11:55:21 +0100

    added test data

---
+++ src/components/FileExplorer.js
@@ -0,0 +1,408 @@
+
+
+const testData = {
+  "data": [
+    {
+      "author": {
+        "id": "29eaee32-4bd7-4079-ae5c-fb9b691752f1",
+        "name": "Alessio",
+        "username": "alessio"
+      },
+      "commit": "MLproject command path fixed",
+      "create_at": "2021-02-12 10:47:29.128000",
+      "file_tree": [
+        {
+          "name": ".gitignore",
+          "version": "0bd4d859-05e1-4603-acd7-5491cfdbb3b2"
+        },
+        {
+          "name": ".results/.gitkeep",
+          "version": "0ea3d835-8007-45eb-8b25-e2472392be53"
+        },
+        {
+          "name": "LICENSE",
+          "version": "b51c9853-d2ca-4a1e-9446-223d3f0c38cd"
+        },
+        {
+          "name": "MLproject",
+          "version": "33e655e7-d3d9-41d6-8c48-a6b915b695fd"
+        },
+        {
+          "name": "Makefile",
+          "version": "998aa20b-fc06-4f37-8caf-3d4e2aa24f4c"
+        },
+        {
+          "name": "README.md",
+          "version": "04109eed-4ee3-45e2-9341-90b14a0971ca"
+        },
+        {
+          "name": "models/.gitkeep",
+          "version": "75f73093-610b-47d0-9518-2ef900f69447"
+        },
+        {
+          "name": "notebooks/.gitkeep",
+          "version": "8bba6ef6-cacf-49b7-9e28-523eabe4128d"
+        },
+        {
+          "name": "notebooks/.ipynb_checkpoints/wine quality-checkpoint.ipynb",
+          "version": "ed9307f2-ed3f-4e73-8cfb-c1b373c4dfcd"
+        },
+        {
+          "name": "notebooks/wine quality.ipynb",
+          "version": "893f1a89-9ef9-45d2-9763-f591f161a33e"
+        },
+        {
+          "name": "references/.gitkeep",
+          "version": "41803c60-bd1c-4fe7-a566-d082a5ec12e2"
+        },
+        {
+          "name": "reports/.gitkeep",
+          "version": "3d9ed2fa-2f38-4f5f-83f4-b3271ebc70fc"
+        },
+        {
+          "name": "reports/figures/.gitkeep",
+          "version": "c15c0ab3-8442-4ace-af71-8222c343932d"
+        },
+        {
+          "name": "requirements.txt",
+          "version": "e781a9e1-b8d1-4fdf-b755-2b0027e599fb"
+        },
+        {
+          "name": "setup.py",
+          "version": "fb586faf-b50e-4b39-932b-27791d6c5c2d"
+        },
+        {
+          "name": "src/__init__.py",
+          "version": "586d526a-ac5e-42ac-9ce1-d86aabd39fa2"
+        },
+        {
+          "name": "src/data/.gitkeep",
+          "version": "dff11a71-1a51-4ad9-81e5-4753eff18983"
+        },
+        {
+          "name": "src/data/__init__.py",
+          "version": "e0aa960c-ef44-4f86-a09c-5618000e28df"
+        },
+        {
+          "name": "src/data/make_dataset.py",
+          "version": "21140814-b9e2-4129-b19a-24d5834a9516"
+        },
+        {
+          "name": "src/features/.gitkeep",
+          "version": "825fd4d5-2979-42d6-8305-2f6d2bd8d4a3"
+        },
+        {
+          "name": "src/features/__init__.py",
+          "version": "5452a953-162c-4314-9fe9-bca7cd6b94fe"
+        },
+        {
+          "name": "src/features/build_features.py",
+          "version": "3027100c-24c0-46de-80af-79a880418035"
+        },
+        {
+          "name": "src/models/.gitkeep",
+          "version": "76fd54ec-9ee4-441f-82b1-0abd62ce0b5f"
+        },
+        {
+          "name": "src/models/__init__.py",
+          "version": "359ae544-34f0-4d84-953d-7b36cdb778ec"
+        },
+        {
+          "name": "src/models/predict_model.py",
+          "version": "0b85dc9a-a611-4c1d-9038-80b5102431d5"
+        },
+        {
+          "name": "src/models/train_model.py",
+          "version": "c2a2d0a3-3287-4e3f-8562-d74fb34d2671"
+        },
+        {
+          "name": "src/visualization/.gitkeep",
+          "version": "cd168d7c-588a-4787-bb81-544765c157e8"
+        },
+        {
+          "name": "src/visualization/__init__.py",
+          "version": "37abfa0f-4c47-456f-96f4-8ae8821d8933"
+        },
+        {
+          "name": "src/visualization/visualize.py",
+          "version": "1eb28b7d-7c90-4adf-b612-ea99d0773466"
+        },
+        {
+          "name": "test_environment.py",
+          "version": "de190c02-df94-4a16-a406-0a632f551175"
+        },
+        {
+          "name": "tox.ini",
+          "version": "7ca90705-cf47-42bd-b061-85d9ead33b9f"
+        }
+      ],
+      "id": "60265cc1b48c2437dcea892e",
+      "project": "6022630de5533e35b8f15366"
+    },
+    {
+      "author": {
+        "id": "29eaee32-4bd7-4079-ae5c-fb9b691752f1",
+        "name": "Alessio",
+        "username": "alessio"
+      },
+      "commit": "Implementazione Wine Quality Model",
+      "create_at": "2021-02-12 10:24:10.712000",
+      "file_tree": [
+        {
+          "name": ".gitignore",
+          "version": "0bd4d859-05e1-4603-acd7-5491cfdbb3b2"
+        },
+        {
+          "name": ".results/.gitkeep",
+          "version": "0ea3d835-8007-45eb-8b25-e2472392be53"
+        },
+        {
+          "name": "LICENSE",
+          "version": "b51c9853-d2ca-4a1e-9446-223d3f0c38cd"
+        },
+        {
+          "name": "MLproject",
+          "version": "da19b1af-91c3-4410-bb39-70779a7ee111"
+        },
+        {
+          "name": "Makefile",
+          "version": "998aa20b-fc06-4f37-8caf-3d4e2aa24f4c"
+        },
+        {
+          "name": "README.md",
+          "version": "04109eed-4ee3-45e2-9341-90b14a0971ca"
+        },
+        {
+          "name": "models/.gitkeep",
+          "version": "75f73093-610b-47d0-9518-2ef900f69447"
+        },
+        {
+          "name": "notebooks/.gitkeep",
+          "version": "8bba6ef6-cacf-49b7-9e28-523eabe4128d"
+        },
+        {
+          "name": "notebooks/.ipynb_checkpoints/wine quality-checkpoint.ipynb",
+          "version": "ed9307f2-ed3f-4e73-8cfb-c1b373c4dfcd"
+        },
+        {
+          "name": "notebooks/wine quality.ipynb",
+          "version": "893f1a89-9ef9-45d2-9763-f591f161a33e"
+        },
+        {
+          "name": "references/.gitkeep",
+          "version": "41803c60-bd1c-4fe7-a566-d082a5ec12e2"
+        },
+        {
+          "name": "reports/.gitkeep",
+          "version": "3d9ed2fa-2f38-4f5f-83f4-b3271ebc70fc"
+        },
+        {
+          "name": "reports/figures/.gitkeep",
+          "version": "c15c0ab3-8442-4ace-af71-8222c343932d"
+        },
+        {
+          "name": "requirements.txt",
+          "version": "e781a9e1-b8d1-4fdf-b755-2b0027e599fb"
+        },
+        {
+          "name": "setup.py",
+          "version": "fb586faf-b50e-4b39-932b-27791d6c5c2d"
+        },
+        {
+          "name": "src/__init__.py",
+          "version": "586d526a-ac5e-42ac-9ce1-d86aabd39fa2"
+        },
+        {
+          "name": "src/data/.gitkeep",
+          "version": "dff11a71-1a51-4ad9-81e5-4753eff18983"
+        },
+        {
+          "name": "src/data/__init__.py",
+          "version": "e0aa960c-ef44-4f86-a09c-5618000e28df"
+        },
+        {
+          "name": "src/data/make_dataset.py",
+          "version": "21140814-b9e2-4129-b19a-24d5834a9516"
+        },
+        {
+          "name": "src/features/.gitkeep",
+          "version": "825fd4d5-2979-42d6-8305-2f6d2bd8d4a3"
+        },
+        {
+          "name": "src/features/__init__.py",
+          "version": "5452a953-162c-4314-9fe9-bca7cd6b94fe"
+        },
+        {
+          "name": "src/features/build_features.py",
+          "version": "3027100c-24c0-46de-80af-79a880418035"
+        },
+        {
+          "name": "src/models/.gitkeep",
+          "version": "76fd54ec-9ee4-441f-82b1-0abd62ce0b5f"
+        },
+        {
+          "name": "src/models/__init__.py",
+          "version": "359ae544-34f0-4d84-953d-7b36cdb778ec"
+        },
+        {
+          "name": "src/models/predict_model.py",
+          "version": "0b85dc9a-a611-4c1d-9038-80b5102431d5"
+        },
+        {
+          "name": "src/models/train_model.py",
+          "version": "c2a2d0a3-3287-4e3f-8562-d74fb34d2671"
+        },
+        {
+          "name": "src/visualization/.gitkeep",
+          "version": "cd168d7c-588a-4787-bb81-544765c157e8"
+        },
+        {
+          "name": "src/visualization/__init__.py",
+          "version": "37abfa0f-4c47-456f-96f4-8ae8821d8933"
+        },
+        {
+          "name": "src/visualization/visualize.py",
+          "version": "1eb28b7d-7c90-4adf-b612-ea99d0773466"
+        },
+        {
+          "name": "test_environment.py",
+          "version": "de190c02-df94-4a16-a406-0a632f551175"
+        },
+        {
+          "name": "tox.ini",
+          "version": "7ca90705-cf47-42bd-b061-85d9ead33b9f"
+        }
+      ],
+      "id": "6026574ab48c2437dcea892d",
+      "project": "6022630de5533e35b8f15366"
+    },
+    {
+      "author": {
+        "id": "29eaee32-4bd7-4079-ae5c-fb9b691752f1",
+        "name": "Alessio",
+        "username": "alessio"
+      },
+      "commit": "Generated by Ilyde",
+      "create_at": "2021-02-09 10:25:17.052000",
+      "file_tree": [
+        {
+          "name": "README.md",
+          "version": "04109eed-4ee3-45e2-9341-90b14a0971ca"
+        },
+        {
+          "name": ".gitignore",
+          "version": "0bd4d859-05e1-4603-acd7-5491cfdbb3b2"
+        },
+        {
+          "name": "test_environment.py",
+          "version": "de190c02-df94-4a16-a406-0a632f551175"
+        },
+        {
+          "name": "setup.py",
+          "version": "fb586faf-b50e-4b39-932b-27791d6c5c2d"
+        },
+        {
+          "name": "requirements.txt",
+          "version": "841349ba-3cf5-4582-885f-0d975954067d"
+        },
+        {
+          "name": "LICENSE",
+          "version": "b51c9853-d2ca-4a1e-9446-223d3f0c38cd"
+        },
+        {
+          "name": "Makefile",
+          "version": "998aa20b-fc06-4f37-8caf-3d4e2aa24f4c"
+        },
+        {
+          "name": "tox.ini",
+          "version": "7ca90705-cf47-42bd-b061-85d9ead33b9f"
+        },
+        {
+          "name": "reports/.gitkeep",
+          "version": "3d9ed2fa-2f38-4f5f-83f4-b3271ebc70fc"
+        },
+        {
+          "name": "reports/figures/.gitkeep",
+          "version": "c15c0ab3-8442-4ace-af71-8222c343932d"
+        },
+        {
+          "name": "notebooks/.gitkeep",
+          "version": "8bba6ef6-cacf-49b7-9e28-523eabe4128d"
+        },
+        {
+          "name": "models/.gitkeep",
+          "version": "75f73093-610b-47d0-9518-2ef900f69447"
+        },
+        {
+          "name": "references/.gitkeep",
+          "version": "41803c60-bd1c-4fe7-a566-d082a5ec12e2"
+        },
+        {
+          "name": "src/__init__.py",
+          "version": "586d526a-ac5e-42ac-9ce1-d86aabd39fa2"
+        },
+        {
+          "name": "src/data/make_dataset.py",
+          "version": "21140814-b9e2-4129-b19a-24d5834a9516"
+        },
+        {
+          "name": "src/data/.gitkeep",
+          "version": "dff11a71-1a51-4ad9-81e5-4753eff18983"
+        },
+        {
+          "name": "src/data/__init__.py",
+          "version": "e0aa960c-ef44-4f86-a09c-5618000e28df"
+        },
+        {
+          "name": "src/visualization/.gitkeep",
+          "version": "cd168d7c-588a-4787-bb81-544765c157e8"
+        },
+        {
+          "name": "src/visualization/__init__.py",
+          "version": "37abfa0f-4c47-456f-96f4-8ae8821d8933"
+        },
+        {
+          "name": "src/visualization/visualize.py",
+          "version": "1eb28b7d-7c90-4adf-b612-ea99d0773466"
+        },
+        {
+          "name": "src/models/predict_model.py",
+          "version": "0b85dc9a-a611-4c1d-9038-80b5102431d5"
+        },
+        {
+          "name": "src/models/train_model.py",
+          "version": "02a5f893-2629-4ce6-b0e4-059e3ff586e4"
+        },
+        {
+          "name": "src/models/.gitkeep",
+          "version": "76fd54ec-9ee4-441f-82b1-0abd62ce0b5f"
+        },
+        {
+          "name": "src/models/__init__.py",
+          "version": "359ae544-34f0-4d84-953d-7b36cdb778ec"
+        },
+        {
+          "name": "src/features/.gitkeep",
+          "version": "825fd4d5-2979-42d6-8305-2f6d2bd8d4a3"
+        },
+        {
+          "name": "src/features/__init__.py",
+          "version": "5452a953-162c-4314-9fe9-bca7cd6b94fe"
+        },
+        {
+          "name": "src/features/build_features.py",
+          "version": "3027100c-24c0-46de-80af-79a880418035"
+        },
+        {
+          "name": ".results/.gitkeep",
+          "version": "0ea3d835-8007-45eb-8b25-e2472392be53"
+        }
+      ],
+      "id": "6022630de5533e35b8f15367",
+      "project": "6022630de5533e35b8f15366"
+    }
+  ],
+  "page_number": 0,
+  "result_per_page": 0,
+  "total": 3
+}
